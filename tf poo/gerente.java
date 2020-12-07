ec72f9ea6c0cc936005142b08b44fcda7c0399a6
Gabriel Martins Gomes <layerbag@users.noreply.github.com>
2020-12-07 17:05:35 -0300

    arquivos_teste

---
+++ tf poo/gerente.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class gerente {
+    ArrayList<tarefa> tarefaDiaria = new ArrayList();
+
+    public static tarefa retorna_tarefa(funcionario f){
+        tarefa a;
+
+    
+    }
+}
