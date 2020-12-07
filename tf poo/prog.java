f1c916b867000b1bd5e3c164fc9d887d76302299
VVSRevolution <68493091+VVSRevolution@users.noreply.github.com>
2020-12-07 17:36:24 -0300

    fix

--- tf poo/prog.java
+++
@@ -1,9 +0,0 @@
-public class Prog {
-    public static void main(final String[] args) {
-        Tarefa t = new Tarefa("tarefa 1");
-        Gerente g = new Gerente();
-        g.adicionTarefa(t);
-        g.removeTarefa(t);
-        
-    }
-}
