ebd5a0ea18cdb948234fce89e42216d392875963
João Victor <joaovictorfe@gmail.com>
2020-12-08 17:21:15 -0300

    novo

--- tf poo/Gerente.java
+++ tf poo/Gerente.java
@@ -1,18 +1,21 @@
 import java.util.ArrayList;
 
 public class Gerente extends Pessoa{
-    private int senhaGerente;
+    private String senhaGerente;
     private String areaSupervisao;
     private ArrayList<Tarefa> tarefas = new ArrayList();
     private ArrayList<Funcionario> funcionarios = new ArrayList();
 
 
-    public Gerente(String cpf, String nome, int idade, double salario, int senhaGerente, String areaSupervisao) {
+    public Gerente(String cpf, String nome, int idade, double salario, String senhaGerente, String areaSupervisao) {
         super(cpf, nome, idade, salario);
         this.senhaGerente = senhaGerente;
         this.areaSupervisao = areaSupervisao;
     }
 
+    public void montaTarefa(){
+        //envia tarefa para arquivo
+    }
 
     public Gerente(int senhaGerente, String areaSupervisao) {
         this.senhaGerente = senhaGerente;
@@ -20,11 +23,11 @@ public class Gerente extends Pessoa{
     }
 
 
-    public int getSenhaGerente() {
+    public String getSenhaGerente() {
         return this.senhaGerente;
     }
 
-    public void setSenhaGerente(int senhaGerente) {
+    public void setSenhaGerente(String senhaGerente) {
         this.senhaGerente = senhaGerente;
     }
 
@@ -36,9 +39,21 @@ public class Gerente extends Pessoa{
         this.areaSupervisao = areaSupervisao;
     }
 
+<<<<<<< Updated upstream
     public void montaTarefa(){
         Tarefa t = new Tarefa(1, 1, 50, false);
         tarefas.add(t);
     }
+=======
+    //recebe array de funcionarios a partir do arquivo Funcionarios.txt
+    //e instancia cada um no arrai do gerente
+    public void leFuncionarios(ArrayList<String> funcionarios){
+        String[] linha;
+        for (String funcionario : funcionarios) {
+            linha = funcionario.split(" ");
+        }
+    }
+    
+>>>>>>> Stashed changes
 
 }
