3013ec111f1494a835c8af7b1f8d246dc2d8d3c9
VVSRevolution <68493091+VVSRevolution@users.noreply.github.com>
2020-12-08 14:20:04 -0300

    CREAT TEST FILE

--- tf poo/Main.java
+++ tf poo/Main.java
@@ -1,5 +1,16 @@
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
+
+        File file = new File ("Save.txt");
+        try {
+            System.out.println(("SAVE.txt criated? " + file.createNewFile()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+      
         Gerente g = new Gerente("2222", "chefe", 55, 5000.00, 123, "Gerente de Projetos");
         Funcionario f = new Funcionario("5555", "JV", 20, 2500.00, 2);
         Tarefa t = new Tarefa(555, 1, 20, false);
